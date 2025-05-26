bb938fc4329aaf6c0a03c9e20eac1b1a71d6c5b7
Dennis Bogers <d.bogers@korsit.com>
2025-05-26 12:43:42 +0200

    fix 0.1.2. sha256

--- Formula/rdslink.rb
+++ Formula/rdslink.rb
@@ -2,7 +2,7 @@ class Rdslink < Formula
     desc "rdslink is a Rust CLI tool that simplifies connecting to AWS RDS instances by automating the setup of a secure tunnel through AWS SSM and EC2 bastion hosts."
     homepage "https://github.com/DenzoNL/rdslink"
     url "https://github.com/DenzoNL/rdslink/releases/download/v0.1.2/rdslink-macos.tar.gz"
-    sha256 "a3d347b5984c4e788825c033212e5a00bb7cc1f9424046bd0e7b57e1c7086a25"
+    sha256 "ae264690da47b27ab9eef4334a8b67b26f311213145a2e4b7ecf7410cc341d6b"
     version "0.1.2"
   
     def install
