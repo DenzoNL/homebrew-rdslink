37f97b7b3b5e54a9eb0399c3741648a964e17a59
Dennis Bogers <d.bogers@korsit.com>
2025-05-27 13:40:52 +0200

    fix: let homebrew infer version

--- Formula/rdslink.rb
+++ Formula/rdslink.rb
@@ -3,7 +3,6 @@ class Rdslink < Formula
     homepage "https://github.com/DenzoNL/rdslink"
     url "https://github.com/DenzoNL/rdslink/releases/download/v0.2.1/rdslink-macos.tar.gz"
     sha256 "e1338389589327d628d1f77c6ee03de9b25865719ecfa3e56a6c466f4e7a28af"
-    version "0.2.0"
   
     def install
       bin.install "rdslink"
